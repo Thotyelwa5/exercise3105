a43ea6679c39ec8c99538bd6db2af2d68d48b51a
Thotyelwa <mpongwanathotyelwa@gmail.com>
2023-05-31 10:32:40 +0200

    q1 done

---
+++ script/code.js
@@ -0,0 +1,14 @@
+let people = [
+   {name : 'Thotyelwa', surname : 'Mpongwana', age: '22'},
+   {name : 'Siyamanga', surname : 'Zweni', age: '22'},
+   {name : 'Kamvelihle', surname: 'Sikalana', age:'20'},
+   {name : 'Aphelele', surname: 'Joyi', age: '21'},
+   {name : 'Xolelwa', surname: 'Hlaletyeni', age: '25'},
+   {name : 'Sisipho', surname : 'Bobo', age: '23'},
+   {name : 'Asanda', surname : 'Qhamfu', age: '23'},
+   {name : 'Sandisiwe', surname: 'Mantshinga', age:'18'},
+   {name : 'Zandisiwe', surname: 'Mkaphuza', age: '24'},
+
+]
+console.log(people);
+
